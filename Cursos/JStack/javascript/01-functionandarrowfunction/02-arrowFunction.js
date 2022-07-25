bc594782ed481c02ade2f8040575f8c7cf3ea271
Luiz Guilherme <luifome@outlook.com>
2022-07-25 18:02:25 -0300

    JStack

---
+++ Cursos/JStack/javascript/01-functionandarrowfunction/02-arrowFunction.js
@@ -0,0 +1,23 @@
+// Arrow Function usa o 'this' do contexto onde ela foi criada
+const minhaArrowFunction = () => {
+  this.lastname = 'Oliveira'
+};
+
+minhaArrowFunction();
+
+// Arrow Function usa o 'arguments' do contexto onde ela foi criada
+ const soma = () => {
+  console.log(arguments);
+}
+soma(1, 2, 3, 4, 5, 6, 7, 'Luiz');
+
+//Short syntax
+const num = 10;
+const getuser = () => ({
+  id: '123',
+  name: 'Luiz',
+});
+
+console.log(getuser());
+
+
