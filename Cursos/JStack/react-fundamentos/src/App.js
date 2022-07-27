e096d33833a261ea6bb54c57eee209175d9b4e37
Luiz Guilherme <luifome@outlook.com>
2022-07-27 13:58:34 -0300

    React Fragments

--- Cursos/JStack/react-fundamentos/src/App.js
+++ Cursos/JStack/react-fundamentos/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function App(){
-  return <h1>Componente App</h1>
+  return (
+    // Formas de usar o react Fragments(<React.Fragments></React.Fragments>, <Fragments></Fragments> ou <></>)
+    <>
+      <h1>Componente App</h1>
+      <h2>Subtítulo</h2>
+    </>
+  );
 }
 
 export default App;
