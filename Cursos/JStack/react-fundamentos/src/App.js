4405b0d14998c5b4aefd8b21a4d990061213a588
Luiz Guilherme <luifome@outlook.com>
2022-07-28 14:45:46 -0300

    React State

--- Cursos/JStack/react-fundamentos/src/App.js
+++ Cursos/JStack/react-fundamentos/src/App.js
@@ -1,28 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import Post from "./Post";
 import Header from "./Header";
 
-const posts = [
-  {title: 'Title#01', subtitle: 'Sub#01', likes: 20},
-  {title: 'Title#02', subtitle: 'Sub#02', likes: 10},
-  {title: 'Title#03', subtitle: 'Sub#02', likes: 50},
-]
+
 
 // Render -> Renderizar
 // Props -> Propriedades
 function App(){
+  const [posts, setPosts] = useState([
+    {id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20},
+    {id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10},
+    {id: Math.random(), title: 'Title#03', subtitle: 'Sub#02', likes: 50},
+  ]);
+
+  function handleRefresh(){
+    setTimeout(() => {
+      setPosts((prevState) => [
+        ...prevState,
+       { 
+        id: Math.random(), 
+        title: `Title#0${prevState.length + 1}`, 
+        subtitle: `Sub#0${prevState.length + 1}`, 
+        likes: 50
+       },
+      ]);
+    }, 2000)
+  }
   return (
     // Formas de usar o react Fragments(<React.Fragments></React.Fragments>, <Fragments></Fragments> ou <></>)
     <>
       
       <Header>
-        <h2>Posts da semana</h2>
+        <h2>
+          Posts da semana
+          <button onClick={handleRefresh} >Atualizar</button>
+          </h2>
       </ Header>
       <hr />
 
       {posts.map(post => (
         <Post
-          key={post.title}
+          key={post.id}
           likes ={post.likes}
           post={{
             title: post.title,
