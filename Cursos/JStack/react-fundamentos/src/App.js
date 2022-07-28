b64cbaec2ead3db93df46b32ba85c79085c78220
Luiz Guilherme <luifome@outlook.com>
2022-07-28 14:08:52 -0300

    Rederização de listas

--- Cursos/JStack/react-fundamentos/src/App.js
+++ Cursos/JStack/react-fundamentos/src/App.js
@@ -2,6 +2,12 @@ import React from "react";
 import Post from "./Post";
 import Header from "./Header";
 
+const posts = [
+  {title: 'Title#01', subtitle: 'Sub#01', likes: 20},
+  {title: 'Title#02', subtitle: 'Sub#02', likes: 10},
+  {title: 'Title#03', subtitle: 'Sub#02', likes: 50},
+]
+
 // Render -> Renderizar
 // Props -> Propriedades
 function App(){
@@ -9,12 +15,23 @@ function App(){
     // Formas de usar o react Fragments(<React.Fragments></React.Fragments>, <Fragments></Fragments> ou <></>)
     <>
       
-      <Header title="Blog do JStack" >
+      <Header>
         <h2>Posts da semana</h2>
       </ Header>
       <hr />
 
-      <Post 
+      {posts.map(post => (
+        <Post
+          key={post.title}
+          likes ={post.likes}
+          post={{
+            title: post.title,
+            subtitle: post.subtitle,
+          }}
+        />
+      ))}
+
+      {/* <Post 
         likes = {20}
         post={{
           title: "Título da Notícia 01", subtitle:"Subtítulo da notícia 01",
@@ -32,7 +49,7 @@ function App(){
           title: "Título da Notícia 03", subtitle:"Subtítulo da notícia 03",
         }}
       />
-      
+       */}
      
     </>
   );
