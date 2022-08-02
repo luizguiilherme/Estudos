5646817400eb7f5a55d4f04fdd21ce40db82e0d1
Luiz Guilherme <luifome@outlook.com>
2022-08-02 11:42:32 -0300

    Theming
    - Implementando Theming com Styled Components
    - Implementando troca de temas
    - Usando o useTheme

--- Cursos/JStack/react-fundamentos/stylede-theming-base/src/styles/themes/dark.js
+++ Cursos/JStack/react-fundamentos/stylede-theming-base/src/styles/themes/dark.js
@@ -4,4 +4,8 @@ export default {
   ...commonStyles,
   backgroundColor: '#222',
   textColor: '#fff',
+  headerBackgroundColor: '#111',
+  footerBackgroundColor: '#111',
+  postBackgroundColor: '#333'
+
 };
