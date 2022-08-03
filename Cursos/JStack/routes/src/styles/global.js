e81c872ddbe6ba68b918d98048658906c2a1c4f0
Luiz Guilherme <luifome@outlook.com>
2022-08-03 16:27:18 -0300

    Delete Cursos/JStack/routes directory

--- Cursos/JStack/routes/src/styles/global.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from 'styled-components';
-
-export default createGlobalStyle`
-  body {
-    background: ${props => props.theme.backgroundColor};
-    color: ${props => props.theme.textColor};
-    font-family: sans-serif;
-  }
-`;
