aec13acfd55dda2cde320bb6eea221c91b5b398e
Luiz Guilherme <luifome@outlook.com>
2022-08-05 13:23:55 -0300

    Configurando o React Router

--- Cursos/JStack/mycontacts/fe/src/components/App/index.js
+++ Cursos/JStack/mycontacts/fe/src/components/App/index.js
@@ -1,22 +1,25 @@
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyles from '../../assests/styles/global';
 import defaultTheme from '../../assests/styles/themes/dafualt';
 
 import Header from '../Header';
-import ContactsList from '../ContactsList';
+import Routes from '../../Routes';
 
 import { Container } from './styles';
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
-      <Container>
-        <Header />
-        <ContactsList />
-      </Container>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+        <Container>
+          <Header />
+          <Routes />
+        </Container>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
