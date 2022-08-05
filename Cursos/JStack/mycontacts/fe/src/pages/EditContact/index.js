b8f4656a02b2c0c225ff277c8afd064497c0edc6
Luiz Guilherme <luifome@outlook.com>
2022-08-05 15:17:39 -0300

    Criando o componente Header

--- Cursos/JStack/mycontacts/fe/src/pages/EditContact/index.js
+++ Cursos/JStack/mycontacts/fe/src/pages/EditContact/index.js
@@ -1,3 +1,9 @@
+import PageHeader from '../../components/PageHeader';
+
 export default function EditContact() {
-  return 'Homepage';
+  return (
+    <PageHeader
+      title="Editar Luiz Guilherme"
+    />
+  );
 }
--- Cursos/JStack/mycontacts/fe/src/pages/NewContact/index.js
+++ Cursos/JStack/mycontacts/fe/src/pages/NewContact/index.js
@@ -1,3 +1,10 @@
+import PageHeader from '../../components/PageHeader';
+
 export default function NewContact() {
-  return 'Homepage';
+  return (
+    <PageHeader
+      title="Novo Contato"
+    />
+
+  );
 }
