8fb06899b763b3a074180e225fd4242a0a7b47a1
Luiz Guilherme <luifome@outlook.com>
2022-08-08 18:54:36 -0300

    Validando dados do input nome e email e exibindo os erros na interface

--- Cursos/JStack/mycontacts/fe/src/components/ContactForm/index.js
+++ Cursos/JStack/mycontacts/fe/src/components/ContactForm/index.js
@@ -1,18 +1,57 @@
 import PropTypes from 'prop-types';
-
 import { useState } from 'react';
+
+import isEmailValid from '../../utils/isEmailValid';
+
 import { Form, ButtonContainer } from './styles';
 
 import FormGroup from '../FormGroup';
 import Input from '../Input';
 import Select from '../Select';
 import Button from '../Button';
+/* eslint-disable react/jsx-no-bind */
 
 export default function ContactForm({ buttonlabel }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [category, setCategory] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setErrors((prevState) => [
+        ...prevState,
+        { field: 'name', message: 'Nome é obrigatório' },
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter((error) => error.field !== 'name'));
+    }
+  }
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      const errorAlreadyExists = errors.find((error) => error.field === 'email');
+
+      if (errorAlreadyExists) {
+        return;
+      }
+      setErrors((prevState) => [
+        ...prevState,
+        { field: 'email', message: 'E-mail inválido' },
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter((error) => error.field !== 'email'));
+    }
+  }
+
+  function getErrorMessageByFieldName(fieldName) {
+    return errors.find((error) => error.field === fieldName)?.message;
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -23,26 +62,23 @@ export default function ContactForm({ buttonlabel }) {
   }
 
   return (
-    // eslint-disable-next-line react/jsx-no-bind
     <Form onSubmit={handleSubmit}>
-
-      <FormGroup>
+      <FormGroup error={getErrorMessageByFieldName('name')}>
         <Input
+          error={getErrorMessageByFieldName('name')}
           placeholder="Nome"
           value={name}
-          onChange={(event) => setName(event.target.value)}
+          onChange={handleNameChange}
         />
-
       </FormGroup>
-
-      <FormGroup>
+      <FormGroup error={getErrorMessageByFieldName('email')}>
         <Input
+          error={getErrorMessageByFieldName('email')}
           placeholder="E-mail"
           value={email}
-          onChange={(event) => setEmail(event.target.value)}
+          onChange={handleEmailChange}
         />
       </FormGroup>
-
       <FormGroup>
         <Input
           placeholder="Telefone"
@@ -50,7 +86,6 @@ export default function ContactForm({ buttonlabel }) {
           onChange={(event) => setPhone(event.target.value)}
         />
       </FormGroup>
-
       <FormGroup>
         <Select
           value={category}
@@ -66,7 +101,6 @@ export default function ContactForm({ buttonlabel }) {
           {buttonlabel}
         </Button>
       </ButtonContainer>
-
     </Form>
   );
 }
