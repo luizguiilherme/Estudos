d131d4e7674331f568b42a8ae7dfc3505733fe93
Luiz Guilherme <luifome@outlook.com>
2022-08-08 16:13:21 -0300

    Pegando Dados e Lidando com submit do form

--- Cursos/JStack/mycontacts/fe/src/components/ContactForm/index.js
+++ Cursos/JStack/mycontacts/fe/src/components/ContactForm/index.js
@@ -1,4 +1,6 @@
 import PropTypes from 'prop-types';
+
+import { useState } from 'react';
 import { Form, ButtonContainer } from './styles';
 
 import FormGroup from '../FormGroup';
@@ -7,26 +9,56 @@ import Select from '../Select';
 import Button from '../Button';
 
 export default function ContactForm({ buttonlabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log({
+      name, email, phone, category,
+    });
+  }
+
   return (
-    <Form>
+    // eslint-disable-next-line react/jsx-no-bind
+    <Form onSubmit={handleSubmit}>
 
       <FormGroup>
-        <Input placeholder="Nome" />
-      </FormGroup>
+        <Input
+          placeholder="Nome"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
 
-      <FormGroup
-        error="O formato do e-mail é inválido"
-      >
-        <Input placeholder="E-mail" error />
-      </FormGroup>
-
-      <FormGroup>
-        <Input placeholder="Telefone" />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Input
+          placeholder="E-mail"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">Categoria</option>
           <option value="Instagram">Instagram</option>
+          <option value="Twitter">Twitter</option>
         </Select>
       </FormGroup>
       <ButtonContainer>
