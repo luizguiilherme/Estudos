8ca4aa4762418b9d6a09f18d057e912af571e880
Luiz Guilherme <luifome@outlook.com>
2022-08-19 15:53:52 -0300

    Criando eventos customizados com a API de CustomEvents do JAvaScript

--- Cursos/JStack/mycontacts/fe/src/pages/NewContact/index.js
+++ Cursos/JStack/mycontacts/fe/src/pages/NewContact/index.js
@@ -1,6 +1,7 @@
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
 import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
 
 export default function NewContact() {
   async function handleSubmit(formData) {
@@ -13,11 +14,17 @@ export default function NewContact() {
 
       };
 
-      const response = await ContactsService.createContact(contact);
+      await ContactsService.createContact(contact);
 
-      console.log(response);
+      toast({
+        type: 'success',
+        text: 'Contato cadastrado com Sucesso!',
+      });
     } catch {
-      alert('Ocorreu um erro ao cadastrar o contato');
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao cadastrar o contato',
+      });
     }
   }
   return (
