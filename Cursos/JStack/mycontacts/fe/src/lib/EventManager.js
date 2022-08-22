db6cd5cb131e9459640cd7e33930462de7d2a572
Luiz Guilherme <luifome@outlook.com>
2022-08-22 15:15:29 -0300

    Implementando método de remoção de event listeners

--- Cursos/JStack/mycontacts/fe/src/lib/EventManager.js
+++ Cursos/JStack/mycontacts/fe/src/lib/EventManager.js
@@ -19,17 +19,37 @@ export default class EventManeger {
       listener(payload);
     });
   }
+
+  removeListener(event, listenerToRemove) {
+    const listerners = this.listeners[event];
+
+    if (!listerners) {
+      return;
+    }
+
+    const filteredListeners = listerners.filter(
+      (listener) => listener !== listenerToRemove,
+
+    );
+
+    this.listerners[event] = filteredListeners;
+  }
 }
 
 const toastEventManeger = new EventManeger();
 
-toastEventManeger.on('addtoast', (payload) => {
+function addToast1(payload) {
   console.log('addtoast listener1', payload);
-});
-toastEventManeger.on('addtoast', (payload) => {
+}
+function addToast2(payload) {
   console.log('addtoast listener2', payload);
-});
+}
 
+toastEventManeger.on('addtoast', addToast1);
+toastEventManeger.on('addtoast', addToast2);
 toastEventManeger.emit('addtoast', { type: 'danger', text: 'Texto' });
+
+toastEventManeger.removeListener('addtoast', addToast1);
+toastEventManeger.emit('addtoast', 'depois de remover');
 
 console.log(toastEventManeger);
